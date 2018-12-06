91178a107916b44d7bafa9c339e603950dd6c2fd
armandleleveur <36453540+armandleleveur@users.noreply.github.com>
2018-12-06 20:28:04 +0100

    Update nouvel.js

--- commands/nouvel.js
+++ commands/nouvel.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const an = '448880510995922944';
 const test = '520300092100444190';
 module.exports.run = async (client, message, args) => {
-
+        let channel = message.guild.channels.find(`name`, "test");
         let a = new Discord.RichEmbed()
         .setTitle('Nouvel An : **Mardi 1 Janvier**\n')
         .setDescription("L'année **2018** est __définitivement__ derrière nous. Maintenant, place à 365 jours de santé, de bonheur et de joies intenses. \n\nBonne année de la part de Drakaria Network et son Staff 🤣\n")
@@ -10,6 +10,6 @@ module.exports.run = async (client, message, args) => {
         .setColor("RAMDOM")
         .setFooter("Nouvel An @Drakaria Network")
         .setTimestamp();
-        client.channels.get(test).send("@everyone");
-        client.channels.get(test).send(a);
+        channel.send('@everyone');
+        channel.send(a);
 }
