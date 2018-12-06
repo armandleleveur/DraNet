214ca6b12be5cfc70e2565bd41a18afa8f4ec236
armandleleveur <36453540+armandleleveur@users.noreply.github.com>
2018-12-06 20:20:21 +0100

    Create nouvel.js

---
+++ commands/nouvel.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const an = '448880510995922944';
+const test = '520300092100444190';
+module.exports.run = async (client, message, args) => {
+
+        let a = new Discord.RichEmbed()
+        .setTitle('Nouvel An : **Mardi 1 Janvier**\n')
+        .setDescription("L'année **2018** est __définitivement__ derrière nous. Maintenant, place à 365 jours de santé, de bonheur et de joies intenses. \n\nBonne année de la part de Drakaria Network et son Staff 🤣\n")
+        .setThumbnail('https://i.imgur.com/gLX824q.png')
+        .setColor("RAMDOM")
+        .setFooter("Nouvel An @Drakaria Network")
+        .setTimestamp();
+        client.channels.get(test).send("@everyone");
+        client.channels.get(test).send(a);
+}
